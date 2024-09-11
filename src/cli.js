6c15334bf7bbec33e73d088f58fead7571e77c1a
alanmulero <alan.mulero@gmail.com>
2024-09-11 17:34:20 -0300

    Implementando libs de gerenciamento de linha com.

--- src/cli.js
+++ src/cli.js
@@ -1,21 +1,60 @@
 import fs from 'fs';
+import path from 'path';  // gerenciamento de caminho relativo e absoluto
 import {trataErros} from "./erros/funcoesErros.js";
 import {contaPalavras}  from "./index.js";
 import {montaSaidaArquivo}  from "./helpers.js";
+import {Command} from 'commander';
 
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2];
-const linkEndereco = caminhoArquivo[3];
-fs.readFile(link
-    , 'utf-8', (erro, texto) => {
-    try {
-        if (erro) throw erro
-        const resultado = contaPalavras(texto);
-        criaESalvaArquivo(resultado,linkEndereco );
-    } catch(erro) {
-        trataErros(erro);
-    }
-})
+
+
+// Usando a biblioteca de terceiros Command.sada para gerenciar linha de comando
+const program = new Command();
+// Chamando os metodos de Command que quero usar.
+program
+    .version('0.0.1')
+    .option('-t, --texto <string>','caminho do texto a ser processado')
+    .option('-d, --destino <string>', 'caminho pasta estino')
+    .action((options) => {
+        const {texto, destino} = options;
+        if(!texto || !destino){
+            console.error('Colocar o caminho de destino.');
+            program.help();
+            return;
+            
+        }
+        // path:
+        const caminhoTexto = path.resolve(texto);
+        const caminhoDestino = path.resolve(destino);
+        try{
+            processaArquivo(caminhoTexto,caminhoDestino);
+            console.log('Texto processado com sucesso');
+            
+
+        }catch(erro){
+            console.log('Ocorreu um erro.', erro);
+            
+        }
+    })
+
+program.parse();    // convertendo os caminhos. 
+
+
+
+// criando uma função para acomodar o readFile
+function processaArquivo(texto, destino){
+    fs.readFile(texto
+        , 'utf-8', (erro, texto) => {
+        try {
+            if (erro) throw erro
+            const resultado = contaPalavras(texto);
+            criaESalvaArquivo(resultado,destino );
+        } catch(erro) {
+            trataErros(erro);
+        }
+    })
+
+}
+
 
 // Salvando os dados em um arquivo.
 //Usando async & await:
